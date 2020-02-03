9c987381af9a069fc697bbbc1f70d6c98f0366d5
Mikkel <mhso@itu.dk>
2020-02-03 17:43:30 +0100

    Did some stuff maybe.

--- src/quiz.py
+++ src/quiz.py
@@ -1,3 +1,4 @@
+import mido
 import numpy as np
 import cv2
 import util
@@ -45,9 +46,11 @@ def draw_sharp(img, x, y):
     cv2.line(img, (x + vert_offset_x, y - vert_offset_y - vert_offset),
              (x + vert_offset_x, y + vert_offset_y), (0, 0, 0), 2)
 
-def draw_semitone(img, x, y):
+def draw_semitone(img, x, y, bass):
     cv2.ellipse(img, (x, y), (14, SPACE_BETWEEN // 2 - 2), 325, 0, 360, (0, 0, 0), -1)
-    cv2.line(img, (x+11, y-SPACE_BETWEEN*3), (x+11, y), (0, 0, 0), 3)
+    offset_y = 1 if bass else -1
+    offset_x = 11 if bass else -10
+    cv2.line(img, (x+offset_x, y-(SPACE_BETWEEN * 3 * offset_y)), (x+offset_x, y), (0, 0, 0), 3)
 
 def draw_note(img, key_index, bass, sharp):
     x = 200
@@ -58,7 +61,7 @@ def draw_note(img, key_index, bass, sharp):
             or (not bass and (key_index < 25 or key_index > 34))):
         line_y = y + 10 if key_index % 2 == 0 else y
         cv2.line(img, (x-15, line_y), (x+15, line_y), (0, 0, 0), 2)
-    draw_semitone(img, x, y)
+    draw_semitone(img, x, y, bass)
     if sharp:
         draw_sharp(img, x-30, y)
 
@@ -99,17 +102,48 @@ def create_image(size):
 
     return img
 
+def wait_for_input(port, note, timelimit):
+    waited_for = 0
+    sleep_time = 10
+    while sleep_time < timelimit:
+        msg = port.receive(True)
+        parsed_msg = util.parse_midi_msg(msg, 0)
+        if parsed_msg is not None:
+            if parsed_msg["key"] == note:
+                return "correct"
+            return "wrong"
+        key = cv2.waitKey(sleep_time)
+        if key == ord('q'):
+            return "quit"
+        waited_for += sleep_time
+    return "out_of_time"
+
 # Draw notes, maybe with a countdown.
 SIZE = (1280, 720)
 # Key range = 0-51. (52 keys)
 # MID C = 23
 
-KEYS = [6, 42, 44, 48, 60, 34]
+KEYS = [39, 42, 44, 48, 60, 34]
 BASSES = [True, True, False, True, True]
-for KEY, BASS in zip(KEYS, BASSES):
-    IMAGE = create_image(SIZE)
-    draw_note(IMAGE, to_sheet_key(KEY), BASS, util.is_sharp(KEY))
-    cv2.imshow("Test", IMAGE)
-    key = cv2.waitKey(0)
-    if key == ord('q'):
-        break
+TIMELIMITS = [5, 4, 4, 4, 3, 3]
+
+IMAGE = create_image(SIZE)
+print(util.get_note_desc(KEYS[0]))
+draw_note(IMAGE, to_sheet_key(KEYS[0]), BASSES[0], util.is_sharp(KEYS[0]))
+cv2.imshow("Test", IMAGE)
+cv2.waitKey(0)
+
+with mido.open_input() as inport:
+    for KEY, BASS, LIMIT in zip(KEYS, BASSES, TIMELIMITS):
+        IMAGE = create_image(SIZE)
+        draw_note(IMAGE, to_sheet_key(KEY), BASS, util.is_sharp(KEY))
+        cv2.imshow("Test", IMAGE)
+        status = wait_for_input(inport, KEY, LIMIT*1000)
+        if status == "correct":
+            print("Correct!")
+        elif result == "wrong":
+            print("WRONG ANSWER!")
+        elif result == "quit":
+            break
+        else:
+            print("TIME RAN OUT!")
